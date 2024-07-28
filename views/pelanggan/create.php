eb1848030e9a0ec41b9eb66b95209062d5dbd7c0
KhunzA <aguero.khunz@gmail.com>
2024-07-28 15:33:38 +0700

    penambahan tampilan edit pelanggan dan CRUD berfungsi

--- views/pelanggan/create.php
+++ views/pelanggan/create.php
@@ -4,26 +4,75 @@
     <?php include_once $base_path."views/bootstrap_header.php"; ?>
     <title>Tambah Pelanggan</title>
 </head>
-<body>
+<body class="d-flex flex-column h-100">
 <?php include_once $base_path."views/page_header.php"; ?>
-<h2 class="h2 mt-5">Tambah Pelanggan</h2>
-<form action="<?= $base_url ?>pelanggan/store" method="POST">
-    <label>ID Pelanggan:</label>
-    <input type="text" name="IdPelanggan"><br>
-    <label>Nama Pengguna:</label>
-    <input type="text" name="NamaPengguna"><br>
-    <label>Password:</label>
-    <input type="password" name="Password"><br>
-    <label>Nama Depan:</label>
-    <input type="text" name="NamaDepan"><br>
-    <label>Nama Belakang:</label>
-    <input type="text" name="NamaBelakang"><br>
-    <label>No HP:</label>
-    <input type="text" name="NoHP"><br>
-    <label>Alamat:</label>
-    <input type="text" name="Alamat"><br>
-    <input type="submit" value="Simpan">
-</form>
+<div class="row">
+    <div class="col-md-4">
+        <h2 class="mt-5">Tambah Pelanggan</h2>
+
+        <form action="<?= $base_url ?>pelanggan/store" method="POST" class="needs-validation" novalidate="">
+
+            <div class="mb-3">
+                <label for="IdPelanggan">ID Pelanggan</label>
+                <input type="text" class="form-control" id="IdPelanggan" name="IdPelanggan" required>
+                <div class="invalid-feedback">
+                    Harap isi ID Pelanggan.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaPengguna">Nama Pengguna</label>
+                <input type="text" class="form-control" id="NamaPengguna" name="NamaPengguna" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Pengguna.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="Password">Password</label>
+                <input type="password" class="form-control" id="Password" name="Password" required>
+                <div class="invalid-feedback">
+                    Harap isi Password.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaDepan">Nama Depan</label>
+                <input type="text" class="form-control" id="NamaDepan" name="NamaDepan" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Depan.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaBelakang">Nama Belakang</label>
+                <input type="text" class="form-control" id="NamaBelakang" name="NamaBelakang" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Belakang.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NoHP">No HP</label>
+                <input type="text" class="form-control" id="NoHP" name="NoHP" required>
+                <div class="invalid-feedback">
+                    Harap isi No HP.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="Alamat">Alamat</label>
+                <input type="text" class="form-control" id="Alamat" name="Alamat" required>
+                <div class="invalid-feedback">
+                    Harap isi Alamat.
+                </div>
+            </div>
+
+            <hr class="mb-4">
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Simpan</button>
+        </form>
+    </div>
+</div>
 <?php include_once $base_path."views/page_footer.php"; ?>
 <?php include_once $base_path."views/bootstrap_footer.php"; ?>
 </body>
--- views/pelanggan/edit.php
+++ views/pelanggan/edit.php
@@ -4,26 +4,75 @@
     <?php include_once $base_path."views/bootstrap_header.php"; ?>
     <title>Edit Pelanggan</title>
 </head>
-<body>
+<body class="d-flex flex-column h-100">
 <?php include_once $base_path."views/page_header.php"; ?>
-<h2 class="h2 mt-5">Edit Pelanggan</h2>
-<form action="<?= $base_url ?>pelanggan/update/<?= $pelanggan['IdPelanggan'] ?>" method="POST">
-    <label>ID Pelanggan:</label>
-    <input type="text" name="IdPelanggan" value="<?= $pelanggan['IdPelanggan'] ?>" readonly><br>
-    <label>Nama Pengguna:</label>
-    <input type="text" name="NamaPengguna" value="<?= $pelanggan['NamaPengguna'] ?>"><br>
-    <label>Password:</label>
-    <input type="password" name="Password" value="<?= $pelanggan['Password'] ?>"><br>
-    <label>Nama Depan:</label>
-    <input type="text" name="NamaDepan" value="<?= $pelanggan['NamaDepan'] ?>"><br>
-    <label>Nama Belakang:</label>
-    <input type="text" name="NamaBelakang" value="<?= $pelanggan['NamaBelakang'] ?>"><br>
-    <label>No HP:</label>
-    <input type="text" name="NoHP" value="<?= $pelanggan['NoHP'] ?>"><br>
-    <label>Alamat:</label>
-    <input type="text" name="Alamat" value="<?= $pelanggan['Alamat'] ?>"><br>
-    <input type="submit" value="Simpan">
-</form>
+<div class="row">
+    <div class="col-md-4">
+        <h2 class="mt-5">Edit Pelanggan</h2>
+
+        <form action="<?= $base_url ?>pelanggan/update/<?= $pelanggan['IdPelanggan'] ?>" method="POST" class="needs-validation" novalidate="">
+
+            <div class="mb-3">
+                <label for="IdPelanggan">ID Pelanggan</label>
+                <input type="text" class="form-control" id="IdPelanggan" name="IdPelanggan" value="<?= $pelanggan['IdPelanggan'] ?>" readonly>
+                <div class="invalid-feedback">
+                    Harap isi ID Pelanggan.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaPengguna">Nama Pengguna</label>
+                <input type="text" class="form-control" id="NamaPengguna" name="NamaPengguna" value="<?= $pelanggan['NamaPengguna'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Pengguna.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="Password">Password</label>
+                <input type="password" class="form-control" id="Password" name="Password" value="<?= $pelanggan['Password'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi Password.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaDepan">Nama Depan</label>
+                <input type="text" class="form-control" id="NamaDepan" name="NamaDepan" value="<?= $pelanggan['NamaDepan'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Depan.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NamaBelakang">Nama Belakang</label>
+                <input type="text" class="form-control" id="NamaBelakang" name="NamaBelakang" value="<?= $pelanggan['NamaBelakang'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi Nama Belakang.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="NoHP">No HP</label>
+                <input type="text" class="form-control" id="NoHP" name="NoHP" value="<?= $pelanggan['NoHP'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi No HP.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="Alamat">Alamat</label>
+                <input type="text" class="form-control" id="Alamat" name="Alamat" value="<?= $pelanggan['Alamat'] ?>" required>
+                <div class="invalid-feedback">
+                    Harap isi Alamat.
+                </div>
+            </div>
+
+            <hr class="mb-4">
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Simpan</button>
+        </form>
+    </div>
+</div>
 <?php include_once $base_path."views/page_footer.php"; ?>
 <?php include_once $base_path."views/bootstrap_footer.php"; ?>
 </body>
