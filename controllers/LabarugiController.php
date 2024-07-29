0797a460e44f1eaf124b5fa7d628e9baaa99f747
Bambang Riswanto <bambang.riswanto@ist.id>
2024-07-29 07:32:03 +0700

    update check login

--- controllers/LabarugiController.php
+++ controllers/LabarugiController.php
@@ -1,6 +1,7 @@
 <?php
 require_once BASE_DIR.'models/Labarugi.php';
 
+CheckLogin::check();
 class LabarugiController extends BaseController {
     public function index() {
         $labarugi = new labarugi();
